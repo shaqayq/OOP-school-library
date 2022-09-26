8eb270e998e9c65fe31450acb02ed7c2df36ae64
shaqayq <shaqayq.darwazi@gmail.com>
2022-09-26 23:17:53 +0430

    change parten constructor

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative 'Person'
 class Student < Person
   def initialize(classroom, age, name = 'unknown', parent_permission: false)
-    super(name, age, parent_permission)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'Person'
 class Teacher < Person
   def initialize(specialization, age, name = 'unknown', parent_permission: false)
-    super(name, age, parent_permission)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
